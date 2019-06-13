d551aae25af7f0e8b1d0045525de1800e6dbe173
Agron Kabashi <agron.kabashi@tretton37.com>
2019-06-13 02:11:18 +0200

    Replaced build pipeline with jspicl-CLI

--- src/game/input.js
+++ src/game/input.js
@@ -4,7 +4,8 @@ export function getInput () {
     rightPressed: btn(1, 0),
     upPressed: btn(2, 0),
     downPressed: btn(3, 0),
-    jumpPressed: btn(4, 0),
+    jumpPressed: !btn(3, 0) && btn(4, 0),
+    dashPressed: btn(3, 0) && btn(4, 0),
     shootPressed: btn(5, 0)
   };
 }
--- src/game/sprites/megaman.js
+++ src/game/sprites/megaman.js
@@ -6,6 +6,6 @@ export const megaman = {
   run: createKeyframeAnimation(2, 4, 0.18, 2, 2),
   jump: createKeyframeAnimation(32, 1, 1, 2, 2),
   shoot: createKeyframeAnimation(0, 0, 0, 2, 2),
-  dash: createKeyframeAnimation(34, 1, 1, 2, 2),
+  dash: createKeyframeAnimation(42, 1, 1, 2, 2),
   elevator: createKeyframeAnimation(0, 0, 0, 0, 2, 2)
 };
